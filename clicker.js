73e42362e1bb1d0561ac1316fe23bc2e21f233d4
hanepyonn66 <168075316+hanepyonn66@users.noreply.github.com>
2024-05-15 14:29:53 +0900

    　

--- clicker.js
+++ clicker.js
@@ -9,12 +9,14 @@ AFRAME.registerComponent('clicker', {
 AFRAME.registerComponent('click_setting1', {
   init: function () {
     this.el.addEventListener('click', e => {
+      //箱の色を変える
       const setting1 = document.querySelector('#setting1');
       setting1.setAttribute('material', 'color: red');
       const setting2 = document.querySelector('#setting2');
       setting2.setAttribute('material', 'color: black');
       const setting3 = document.querySelector('#setting3');
       setting3.setAttribute('material', 'color: black');
+      //テキストとその色を変える
 
     });
   }
